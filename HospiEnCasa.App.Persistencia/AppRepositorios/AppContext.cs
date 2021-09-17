8103893ad4c5877e4fb0053366407feb39b2d0c3
Heri <hlondono@misena.edu.com>
2021-09-17 08:58:01 -0500

    Segunda migracion

--- HospiEnCasa.App.Persistencia/AppRepositorios/AppContext.cs
+++ HospiEnCasa.App.Persistencia/AppRepositorios/AppContext.cs
@@ -5,6 +5,14 @@ namespace HospiEnCasa.App.Persistencia
     public class AppContext : DbContext
     {
         public DbSet<Persona> Personas{get; set;}
+        public DbSet<Paciente> Pacientes{get; set;}
+        public DbSet<Medico> Medicos{get; set;}
+        public DbSet<Enfermera> Enfermeras{get; set;}
+        public DbSet<FamiliarDesignado> FamiliaresDesignados{get; set;}
+        public DbSet<SignoVital> SignosVitales{get; set;}
+        public DbSet<Historia> Historias{get; set;}
+        public DbSet<SugerenciaCuidado> SugerenciasCuidado{get; set;}
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             if(!optionsBuilder.IsConfigured)
