407ebb0572e03fbfa648bbe54c98d6ed92f3ff97
Heri <hlondono@misena.edu.com>
2021-09-17 08:34:08 -0500

    Segundo commit

--- HospiEnCasa.App.Consola/Program.cs
+++ HospiEnCasa.App.Consola/Program.cs
@@ -6,7 +6,7 @@ namespace HospiEnCasa.App.Consola
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");            
         }
     }
 }
