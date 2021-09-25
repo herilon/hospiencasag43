9709369cdb0fb003b1ca0995c45e9e8afcbefaa6
Heri <hlondono@misena.edu.co>
2021-09-25 11:09:31 -0500

    Detalle de pacientes

--- HospiEnCasa.App.Frontend/Pages/Pacientes/Details.cshtml.cs
+++ HospiEnCasa.App.Frontend/Pages/Pacientes/Details.cshtml.cs
@@ -4,13 +4,31 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using HospiEnCasa.App.Dominio;
+using HospiEnCasa.App.Persistencia;
 
 namespace HospiEnCasa.App.Frontend.Pages.Pacientes
 {
     public class DetailsModel : PageModel
     {
-        public void OnGet()
+        private readonly IRepositorioPaciente _repoPaciente;
+        public Paciente paciente {get; set;}
+
+        public DetailsModel(IRepositorioPaciente repoPaciente)
         {
+            _repoPaciente = repoPaciente;
+        }
+        public IActionResult OnGet(int id)
+        {
+            paciente = _repoPaciente.GetPaciente(id);
+            if (paciente == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Page();
+            }
         }
     }
 }
