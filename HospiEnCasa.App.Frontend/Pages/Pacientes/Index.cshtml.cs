4acc2016c27d4d592ccb3a363426bb07cb1016b4
Heri <hlondono@misena.edu.co>
2021-09-25 10:19:50 -0500

    Index de pacientes

--- HospiEnCasa.App.Frontend/Pages/Pacientes/Index.cshtml.cs
+++ HospiEnCasa.App.Frontend/Pages/Pacientes/Index.cshtml.cs
@@ -4,13 +4,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using HospiEnCasa.App.Dominio;
+using HospiEnCasa.App.Persistencia;
 
 namespace HospiEnCasa.App.Frontend.Pages.Pacientes
 {
     public class IndexModel : PageModel
     {
+        private readonly IRepositorioPaciente _repoPaciente;
+        public IEnumerable<Paciente> pacientes {get; set;}
+
+        public IndexModel(IRepositorioPaciente repoPaciente)
+        {
+            _repoPaciente = repoPaciente;
+        }
         public void OnGet()
         {
+            pacientes = _repoPaciente.GetAllPacientes();
         }
     }
 }
