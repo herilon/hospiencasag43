cd16c9b117200c670b40084bc5a1dfb4b2f5ef90
Heri <hlondono@misena.edu.co>
2021-10-01 09:24:47 -0500

    Edit Paciente

--- HospiEnCasa.App.Dominio/Entidades/Persona.cs
+++ HospiEnCasa.App.Dominio/Entidades/Persona.cs
@@ -1,9 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace HospiEnCasa.App.Dominio
 {
     public class Persona
     {
         // Identificador único de cada persona
         public int Id { get; set; }
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(50, ErrorMessage = "Maximo 50 caracteres")]
         public string Nombre { get; set; }
         public string Apellidos { get; set; }
         public string NumeroTelefono { get; set; }
--- HospiEnCasa.App.Frontend/Pages/Pacientes/Create.cshtml.cs
+++ HospiEnCasa.App.Frontend/Pages/Pacientes/Create.cshtml.cs
@@ -26,8 +26,15 @@ namespace HospiEnCasa.App.Fontend.Pages.Pacientes
 
         public IActionResult OnPost(Paciente paciente)
         {
-            _repoPaciente.AddPaciente(paciente);
-            return RedirectToPage("Index");
+            if (ModelState.IsValid)
+            {
+                _repoPaciente.AddPaciente(paciente);
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return Page();
+            }
         }
     }
 }
